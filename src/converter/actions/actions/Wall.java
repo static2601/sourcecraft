23fedabcc521ac6ce451c454e21d1e6d58426328
statiic <staticx2601@yahoo.com>
2024-07-25 19:21:07 -0400

    Improved wall connections

--- src/converter/actions/actions/Wall.java
+++ src/converter/actions/actions/Wall.java
@@ -1,5 +1,6 @@
 package converter.actions.actions;
 
+import basic.Loggger;
 import converter.actions.Action;
 import converter.mapper.Mapper;
 import minecraft.Block;
@@ -39,302 +40,141 @@ public class Wall extends Action {
 		end = bestX;
 		
 		String str = "";
-		if(east.equals("true")) str += "E";
-		if(west.equals("true")) str += "W";
-		if(north.equals("true")) str += "N";
-		if(south.equals("true")) str += "S";
+		boolean low = false;
+		if(east.equals("tall") || east.equals("low")) str += "E";
+		if(west.equals("tall") || west.equals("low")) str += "W";
+		if(north.equals("tall") || north.equals("low")) str += "N";
+		if(south.equals("tall") || south.equals("low")) str += "S";
+		//low wall
+		if(east.equals("low")) low = true;
+		if(west.equals("low")) low = true;
+		if(north.equals("low")) low = true;
+		if(south.equals("low")) low = true;
+		// post
 		if(up.equals("true")) str += "U";
-		//Loggger.log("DEBUG:  Pane Directions " + str);
-			
+		Loggger.log("DEBUG:  Pane Directions " + str);
+
+
+
 		switch (str) {
 			case "EU":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				offset =         new Position(12, 0, 5);
-				negativeOffset = new Position(0, 2, 5);
-				end = bestX;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
+				ETall(context, p, material, bestX, low);
 				context.markAsConverted(p, end);
 				break;
 				
 			case "WU":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				offset =         new Position(0, 0, 5);
-				negativeOffset = new Position(12, 2, 5);
-				end = bestX;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
+				WTall(context, p, material, bestX, low);
 				context.markAsConverted(p, end);
 				break;
 				
 			case "NU":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				offset =         new Position(5, 0, 0);
-				negativeOffset = new Position(5, 2, 12);
-				end = bestZ;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
+				NTall(context, p, material, bestZ, low);
 				context.markAsConverted(p, end);
 				break;
 				
 			case "SU":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				offset =         new Position(5, 0, 12);
-				negativeOffset = new Position(5, 2, 0);
-				end = bestZ;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
+				STall(context, p, material, bestZ, low);
 				context.markAsConverted(p, end);
 				break;
 			
 			//up position, single post	
 			case "U":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
 				context.markAsConverted(p, end);
 				break;
 				
 			//east to west full pane
 			case "EW":
-				offset =         new Position(0, 0, 5);
-				negativeOffset = new Position(0, 2, 5);
-				end = bestX;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				EWTall(context, p, material, bestX, low);
 				context.markAsConverted(p, end);
 				break;
 				
 			//north to south full pane
 			case "NS":
-				offset =         new Position(5, 0, 0);
-				negativeOffset = new Position(5, 2, 0);
-				end = bestZ;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				NSTall(context, p, material, bestZ, low);
 				context.markAsConverted(p, end);
 				break;
 				
 			//corner 
 			case "ENU":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				//north piece
-				offset =         new Position(5, 0, 0);
-				negativeOffset = new Position(5, 2, 12);
-				end = bestZ;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				//east piece
-				offset =         new Position(12, 0, 5);
-				negativeOffset = new Position(0, 2, 5);
-				end = bestZ;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
+				ETall(context, p, material, bestZ, low);
+				NTall(context, p, material, bestZ, low);
 				context.markAsConverted(p, end);
 				break;
 				
 			case "WNU":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				//north piece
-				offset =         new Position(5, 0, 0);
-				negativeOffset = new Position(5, 2, 12);
-				end = bestZ;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				//west piece
-				offset =         new Position(0, 0, 5);
-				negativeOffset = new Position(12, 2, 5);
-				end = bestX;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
+				WTall(context, p, material, bestX, low);
+				NTall(context, p, material, bestZ, low);
 				context.markAsConverted(p, end);
 				break;
 				
 			case "ESU":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				//south piece
-				offset =         new Position(5, 0, 12);
-				negativeOffset = new Position(5, 2, 0);
-				end = bestZ;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				//east piece
-				offset =         new Position(12, 0, 5);
-				negativeOffset = new Position(0, 2, 5);
-				end = bestX;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
+				ETall(context, p, material, bestZ, low);
+				STall(context, p, material, bestX, low);
 				context.markAsConverted(p, end);
 				break;
 				
 			case "WSU":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				//south piece
-				offset =         new Position(5, 0, 12);
-				negativeOffset = new Position(5, 2, 0);
-				end = bestZ;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				//west piece
-				offset =         new Position(0, 0, 5);
-				negativeOffset = new Position(12, 2, 5);
-				end = bestX;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
+				WTall(context, p, material, bestZ, low);
+				STall(context, p, material, bestX, low);
 				context.markAsConverted(p, end);
 				break;
 			
 			//full wall with post
 			case "NSU":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				//north to south piece
-				offset =         new Position(5, 0, 0);
-				negativeOffset = new Position(5, 2, 0);
-				end = bestZ;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
+				NSTall(context, p, material, bestZ, low);
 				context.markAsConverted(p, end);
 				break;
 			
 			case "EWU":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				//north to south piece
-				offset =         new Position(0, 0, 5);
-				negativeOffset = new Position(0, 2, 5);
-				end = bestZ;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
+				EWTall(context, p, material, bestZ, low);
 				context.markAsConverted(p, end);
 				break;
 				
 			//full wall with extension
 			case "EWNU":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				//full pane east to west
-				offset =         new Position(0, 0, 5);
-				negativeOffset = new Position(0, 2, 5);
-				end = bestX;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-						
-				//north piece
-				offset =         new Position(5, 0, 0);
-				negativeOffset = new Position(5, 2, 12);
-				end = bestZ;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
+				EWTall(context, p, material, bestX, low);
+				NTall(context, p, material, bestZ, low);
 				context.markAsConverted(p, end);
 				break;
 				
 			case "EWSU":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				//full pane east to west
-				offset =         new Position(0, 0, 5);
-				negativeOffset = new Position(0, 2, 5);
-				end = bestX;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-						
-				//south piece
-				offset =         new Position(5, 0, 12);
-				negativeOffset = new Position(5, 2, 0);
-				end = bestZ;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
+				EWTall(context, p, material, bestX, low);
+				STall(context, p, material, bestZ, low);
 				context.markAsConverted(p, end);
 				break;
 				
 			case "ENSU":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				//full pane north to south
-				offset =         new Position(5, 0, 0);
-				negativeOffset = new Position(5, 2, 0);
-				end = bestZ;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-						
-				//east piece
-				offset =         new Position(12, 0, 5);
-				negativeOffset = new Position(0, 2, 5);
-				end = bestX;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
+				NSTall(context, p, material, bestZ, low);
+				ETall(context, p, material, bestX, low);
 				context.markAsConverted(p, end);
 				break;
 				
 			case "WNSU":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				//full pane north to south
-				offset =         new Position(5, 0, 0);
-				negativeOffset = new Position(5, 2, 0);
-				end = bestZ;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-						
-				//west piece
-				offset =         new Position(0, 0, 5);
-				negativeOffset = new Position(12, 2, 5);
-				end = bestX;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
+				NSTall(context, p, material, bestZ, low);
+				WTall(context, p, material, bestX, low);
 				context.markAsConverted(p, end);
 				break;
 			
 			//four ways
 			case "EWNSU":
-				offset =         new Position(4, 0, 4);
-				negativeOffset = new Position(4, 0, 4);
-				end = bestY;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-				
-				//full pane east to west
-				offset =         new Position(0, 0, 5);
-				negativeOffset = new Position(0, 2, 5);
-				end = bestX;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
-						
-				//full north to south
-				offset =         new Position(5, 0, 0);
-				negativeOffset = new Position(5, 2, 0);
-				end = bestZ;
-				context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
+				Post(context, p, material, bestY);
+				NSTall(context, p, material, bestZ, low);
+				EWTall(context, p, material, bestX, low);
 				context.markAsConverted(p, end);
 				break;
 			
@@ -352,4 +192,97 @@ public class Wall extends Action {
 		//context.addDetail(context.createCuboid(p, end, parts, offset, negativeOffset, material));
 		//context.markAsConverted(p, end);
 	}
+	private void Post(Mapper c, Position p, Block m, Position end) {
+		int parts = 16;
+		Position offset =         new Position(4, 0, 4);
+		Position negativeOffset = new Position(4, 0, 4);
+		c.addDetail(c.createCuboid(p, end, parts, offset, negativeOffset, m));
+	}
+	private void EWLow(Mapper c, Position p, Block m, Position end) {
+		int parts = 16;
+		Position offset =         new Position(0, 0, 5);
+		Position negativeOffset = new Position(0, 2, 5);
+		c.addDetail(c.createCuboid(p, end, parts, offset, negativeOffset, m));
+	}
+	private void NSLow(Mapper c, Position p, Block m, Position end) {
+		int parts = 16;
+		Position offset =         new Position(5, 0, 0);
+		Position negativeOffset = new Position(5, 2, 0);
+		c.addDetail(c.createCuboid(p, end, parts, offset, negativeOffset, m));
+	}
+	private void EWTall(Mapper c, Position p, Block m, Position end, boolean low) {
+		int parts = 16;
+		int y = 0;
+		if(low) y = 2;
+		Position offset =         new Position(0, 0, 5);
+		Position negativeOffset = new Position(0, y, 5);
+		c.addDetail(c.createCuboid(p, end, parts, offset, negativeOffset, m));
+	}
+	private void NSTall(Mapper c, Position p, Block m, Position end, boolean low) {
+		int parts = 16;
+		int y = 0;
+		if(low) y = 2;
+		Position offset =         new Position(5, 0, 0);
+		Position negativeOffset = new Position(5, y, 0);
+		c.addDetail(c.createCuboid(p, end, parts, offset, negativeOffset, m));
+	}
+	//corners - low
+	private void ELow(Mapper c, Position p, Block m, Position end) {
+		int parts = 16;
+		Position offset =         new Position(12, 0, 5);
+		Position negativeOffset = new Position(0, 2, 5);
+		c.addDetail(c.createCuboid(p, end, parts, offset, negativeOffset, m));
+	}
+	private void WLow(Mapper c, Position p, Block m, Position end) {
+		int parts = 16;
+		Position offset =         new Position(0, 0, 5);
+		Position negativeOffset = new Position(12, 2, 5);
+		c.addDetail(c.createCuboid(p, end, parts, offset, negativeOffset, m));
+	}
+	private void NLow(Mapper c, Position p, Block m, Position end) {
+		int parts = 16;
+		Position offset =         new Position(5, 0, 0);
+		Position negativeOffset = new Position(5, 2, 12);
+		c.addDetail(c.createCuboid(p, end, parts, offset, negativeOffset, m));
+	}
+	private void SLow(Mapper c, Position p, Block m, Position end) {
+		int parts = 16;
+		Position offset =         new Position(5, 0, 12);
+		Position negativeOffset = new Position(5, 2, 0);
+		c.addDetail(c.createCuboid(p, end, parts, offset, negativeOffset, m));
+	}
+	//corners - tall
+	private void ETall(Mapper c, Position p, Block m, Position end, boolean low) {
+		int parts = 16;
+		int y = 0;
+		if(low) y = 2;
+		Position offset =         new Position(12, 0, 5);
+		Position negativeOffset = new Position(0, y, 5);
+		c.addDetail(c.createCuboid(p, end, parts, offset, negativeOffset, m));
+	}
+	private void WTall(Mapper c, Position p, Block m, Position end, boolean low) {
+		int parts = 16;
+		int y = 0;
+		if(low) y = 2;
+		Position offset =         new Position(0, 0, 5);
+		Position negativeOffset = new Position(12, y, 5);
+		c.addDetail(c.createCuboid(p, end, parts, offset, negativeOffset, m));
+	}
+	private void NTall(Mapper c, Position p, Block m, Position end, boolean low) {
+		int parts = 16;
+		int y = 0;
+		if(low) y = 2;
+		Position offset =         new Position(5, 0, 0);
+		Position negativeOffset = new Position(5, y, 12);
+		c.addDetail(c.createCuboid(p, end, parts, offset, negativeOffset, m));
+	}
+	private void STall(Mapper c, Position p, Block m, Position end, boolean low) {
+		int parts = 16;
+		int y = 0;
+		if(low) y = 2;
+		Position offset =         new Position(5, 0, 12);
+		Position negativeOffset = new Position(5, y, 0);
+		c.addDetail(c.createCuboid(p, end, parts, offset, negativeOffset, m));
+	}
+
 }

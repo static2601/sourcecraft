26bb18e93b10f4eb88f5a42f1df29e98f978fb33
statiic <staticx2601@yahoo.com>
2024-07-27 22:49:36 -0400

    Added skins, solids for methods in CustomActionManager

--- src/vmfWriter/entity/pointEntity/pointEntity/PropStatic.java
+++ src/vmfWriter/entity/pointEntity/pointEntity/PropStatic.java
@@ -8,11 +8,56 @@ import vmfWriter.entity.pointEntity.PointEntity;
 
 public class PropStatic extends PointEntity {
 
+	private int solid;
+	private int skin;
+	private int shadows;
+
 	private String model;
 	private Angles angles;
 
+	// me
+	public PropStatic(String model, int skin) {
+		this(model, new Angles());
+		this.skin = skin;
+	}
+
 	public PropStatic(String model) {
 		this(model, new Angles());
+	}
+
+	public void setSkin(int skin) {
+		this.skin = skin;
+	}
+
+	public void setSkin(int skin, int solid) {
+		this.skin = skin;
+		this.solid = solid;
+	}
+
+	/**
+	* not solid = 0, use bounding box = 2, use vphysics = 6
+	*/
+	public void setSolid(int solid) {
+		this.solid = solid;
+	}
+
+	public void disableShadows(int shadows) {
+		this.shadows = shadows;
+	}
+
+	public PropStatic(String model, Angles angles, int skin, int solid) {
+		this.model = model;
+		this.angles = angles.copy();
+		this.skin = skin;
+		this.solid = solid;
+	}
+
+	public PropStatic(String model, Angles angles, int skin, int solid, int shadows) {
+		this.model = model;
+		this.angles = angles.copy();
+		this.skin = skin;
+		this.solid = solid;
+		this.shadows = shadows;
 	}
 
 	public PropStatic(String model, Angles angles) {
@@ -30,7 +75,7 @@ public class PropStatic extends PointEntity {
 
 	@Override
 	public PropStatic create(Position origin) {
-		PropStatic result = new PropStatic(this.model, this.angles);
+		PropStatic result = new PropStatic(this.model, this.angles, this.skin, this.solid, this.shadows);
 		result.setOrigin(origin);
 		return result;
 	}
@@ -44,7 +89,7 @@ public class PropStatic extends PointEntity {
 	public void writeVmfSpecific(vmfWriter.ValveWriter writer) throws IOException {
 		writer.put("angles", this.angles)
 				.put("disableselfshadowing", false)
-				.put("disableshadows", false)
+				.put("disableshadows", this.shadows)
 				.put("disablevertexlighting", false)
 				.put("fademaxdist", 0)
 				.put("fademindist", -1)
@@ -54,7 +99,7 @@ public class PropStatic extends PointEntity {
 				.put("mindxlevel", 0)
 				.put("model", this.model)
 				.put("screenspacefade", 0)
-				.put("skin", 0)
-				.put("solid", 0);
+				.put("skin", this.skin)
+				.put("solid", this.solid);
 	}
 }

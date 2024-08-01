95707e6746fa8c1f2fc85a730baae6a266662777
statiic <staticx2601@yahoo.com>
2024-07-31 22:31:56 -0400

    Added crop models

---
+++ src/converter/actions/actions/CropModel.java
@@ -0,0 +1,55 @@
+package converter.actions.actions;
+
+import converter.actions.Action;
+import converter.mapper.Mapper;
+import minecraft.Block;
+import minecraft.Position;
+import minecraft.Property;
+import vmfWriter.entity.pointEntity.pointEntity.PropStatic;
+
+public class CropModel extends Action {
+
+	// skin, make index for all skin names
+	private int skin;
+	// not solid = 0, use bounding box = 2, use vphysics = 6
+	private int solid;
+
+	private static String MODEL = "models/props/minecraft_original/CropModel1.mdl";
+	private PropStatic crossModel = new PropStatic(CropModel.MODEL);
+
+	/**
+	 * Add Plant model instance with selected skin
+	 *
+	 * @skin skin index
+	 * @solid not solid = 0, use bounding box = 2, use vphysics = 6
+	 */
+	public CropModel(int skin, int solid) {
+		this.skin = skin;
+		this.solid = solid;
+		this.crossModel.setSkin(skin, solid);
+	}
+
+	@Override
+	public void add(Mapper context, Position p, Block material) {
+		int skin = this.skin;
+
+		// crop age
+		String age = material.getProperty(Property.valueOf("age"));
+		if (age != null) {
+			skin += Integer.parseInt(age);
+		}
+
+		context.setPointToGrid(p);
+		context.movePointInGridDimension(0.5, 0, 0.5);
+
+		//turn off random rotation for now, later set per object type
+		//int verticalRotation = (int) (Math.random() * 360);
+		int verticalRotation = 0;
+		crossModel.getAngles()
+				.setY(verticalRotation);
+		crossModel.setSkin(skin);
+		crossModel.setSolid(this.solid);
+		context.addPointEntity(crossModel);
+		context.markAsConverted(p);
+	}
+}

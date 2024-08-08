946475c9f7403c79aaea9220960ed364a4f6decf
static2601 <staticx2601@yahoo.com>
2024-08-08 14:17:37 -0400

    Updated and added hanging lantern. Added soul lantern skin.

--- src/converter/actions/actions/LanternGround.java
+++ src/converter/actions/actions/LanternGround.java
@@ -1,9 +1,11 @@
 package converter.actions.actions;
 
+import basic.Loggger;
 import converter.actions.Action;
 import converter.mapper.Mapper;
 import minecraft.Block;
 import minecraft.Position;
+import minecraft.Property;
 import vmfWriter.entity.pointEntity.pointEntity.PropStatic;
 
 /**
@@ -12,14 +14,26 @@ import vmfWriter.entity.pointEntity.pointEntity.PropStatic;
  */
 public class LanternGround extends Action {
 
-	private final static String MODEL = "models/props/minecraft_original/lantern.mdl";;
-	private final static PropStatic LANTERN_GROUND = new PropStatic(LanternGround.MODEL);
+	private final static String MODEL = "models/props/minecraft_original/lantern.mdl";
+	private final static String MODEL2 = "models/props/minecraft_original/lanternHanging.mdl";
+	private final static PropStatic lantern = new PropStatic(MODEL);
 
 	@Override
 	public void add(Mapper context, Position p, Block material) {
 		context.setPointToGrid(p);
 		context.movePointInGridDimension(0.5, 0, 0.5);
-		context.addPointEntity(LanternGround.LANTERN_GROUND);
+
+		int skin = 0;
+		if(material.getName().endsWith("soul_lantern"))
+			skin = 1;
+		Loggger.log(material.get()+"");
+		String hanging = material.getProperty(Property.hanging);
+		if(hanging.equals("true"))
+			lantern.setModel(MODEL2);
+		else lantern.setModel(MODEL);
+
+		lantern.setSkin(skin);
+		context.addPointEntity(lantern);
 		context.markAsConverted(p);
 	}
 

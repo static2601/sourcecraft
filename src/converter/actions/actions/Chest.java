efbfed073bb001f922f0b3b0e7cc11da36e5da6a
static2601 <staticx2601@yahoo.com>
2024-08-08 13:43:40 -0400

    Added model option

--- src/converter/actions/actions/Chest.java
+++ src/converter/actions/actions/Chest.java
@@ -9,18 +9,55 @@ import minecraft.Position;
 import minecraft.Property;
 import vmfWriter.Cuboid;
 import vmfWriter.Skin;
+import vmfWriter.entity.pointEntity.pointEntity.PropStatic;
 
 public class Chest extends Action {
 
+	private static String MODEL = "models/props/minecraft_original/SingleChest.mdl";
+	private PropStatic chest = new PropStatic(MODEL);
+	private static boolean useModel = true;
+
 	@Override
 	public void add(Mapper context, Position pos, Block block) {
-		Position end = pos;
-		int pixels = 16;
-		Position startOffset = new Position(1, 0, 1);
-		Position endOffset = new Position(1, 2, 1);
-		Cuboid chest = context.createCuboid(pos, end, pixels, startOffset, endOffset, block);
-		chest.setSkin(this.constructSkin(block));
-		context.addDetail(chest);
+		if(useModel) {
+			context.setPointToGrid(pos);
+			context.movePointInGridDimension(0.5, 0, 0.5);
+			int rotation = 0;
+			String facing = block.getProperty(Property.facing);
+			String type = block.getProperty(Property.type);
+			int skin = 0;
+			if(block.getName().endsWith("chest")) skin = 0;
+			else if(block.getName().endsWith("ender_chest")) skin = 1;
+			else if(block.getName().endsWith("trapped_chest")) skin = 2;
+
+			if(facing.equals("south")) {
+				rotation = 180;
+			}
+			if(facing.equals("east")) {
+				rotation = -90;
+			}
+			if(facing.equals("north")) {
+				rotation = 0;
+			}
+			if(facing.equals("west")) {
+				rotation = -270;
+			}
+
+			chest.getAngles().setY(rotation);
+			chest.setSkin(skin);
+			chest.setSolid(6);
+			context.addPointEntity(chest);
+			context.markAsConverted(pos);
+
+		} else {
+			Position end = pos;
+			int pixels = 16;
+			Position startOffset = new Position(1, 0, 1);
+			Position endOffset = new Position(1, 2, 1);
+			Cuboid chest = context.createCuboid(pos, end, pixels, startOffset, endOffset, block);
+			chest.setSkin(this.constructSkin(block));
+			context.addDetail(chest);
+		}
 	}
 
 	private Skin constructSkin(Block block) {

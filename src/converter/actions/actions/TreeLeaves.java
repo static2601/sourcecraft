0f324e4d25b6fc9c990a92f18299dbf12cc4c0e4
Statiic <staticx2601@yahoo.com>
2025-04-24 04:18:05 -0400

    Added options for different types of tree leaves. Use as a model, use as a brush and combined and optimized, use completely unoptimized for use with propper. Options can be selected in options.json.

--- src/converter/actions/actions/TreeLeaves.java
+++ src/converter/actions/actions/TreeLeaves.java
@@ -4,38 +4,42 @@ import converter.actions.Action;
 import converter.mapper.Mapper;
 import minecraft.Block;
 import minecraft.Position;
+import periphery.Option;
 import vmfWriter.entity.pointEntity.pointEntity.PropStatic;
 
 public class TreeLeaves extends Action {
 
-	// skin, make index for all skin names
 	private int skin;
-	// not solid = 0, use bounding box = 2, use vphysics = 6
 	private int solid;
-	public boolean useModel = false;
-	public boolean combined = false;
+	public boolean useModel;
+	public boolean brushTrimmed;
 	public boolean addAsSolid = false;
+	public boolean brushOptimized;
+	public boolean setForPropper = false;
 
 	private static final String MODEL = "models/props/minecraft_original/Tree_Leaves.mdl";
 	private final PropStatic treeLeaves = new PropStatic(TreeLeaves.MODEL);
 
-	/**
-	 * Add Plant model instance with selected skin
-	 * 
-	 * @skin skin index
-	 * @solid not solid = 0, use bounding box = 2, use vphysics = 6
-	 */
-	public TreeLeaves(int skin, int solid) {
-		this.skin = skin;
-		this.solid = solid;
-		this.treeLeaves.setSkin(skin, solid); // set in PropStatic.java
+	public TreeLeaves() {
+		Option option = getOptions(this.getName().toLowerCase());
+		useModel = option.get_useModel();
+		brushOptimized = option.get_brushOptimized();
+		brushTrimmed = option.get_brushTrimmed();
+		setForPropper = option.get_setForPropper();
+
+		// if brushOptimized=true, brushTrimmed is false can only true if brushOptimized=false
+		// if trees need to be converted to a model with propper, would need set in GUI
+		// brushTrimmed=false, useModel=false, brushOptimized=false
+		if (setForPropper) leavesUnoptimized();
+		// else run as normal, with other options
+
 	}
-	public TreeLeaves(int skin) {
-		this.skin = skin;
-		this.solid = 6;
-		this.treeLeaves.setSkin(skin, solid); // set in PropStatic.java
+	private void leavesUnoptimized() {
+		addAsSolid = true;
+		useModel = false;
+		brushTrimmed = false;
+		brushOptimized = false;
 	}
-	public TreeLeaves() {}
 
 	@Override
 	public void add(Mapper context, Position p, Block material) {
@@ -52,24 +56,21 @@ public class TreeLeaves extends Action {
 			context.markAsConverted(p);
 
 		} else {
-			if (!combined) {
-				Position end = context.getCuboidFinder()
-						.getBestXY(p, material);
+			if (!brushOptimized && brushTrimmed) {
+
 				// making leaves slightly smaller on all sides prevents faces from culling
 				// when the map is compiled, else the combined and look bad
 				//int parts = 8;
-				int parts = 256;
+				int parts = 512;
 				Position offset = new Position(0, 0, 0);
 				Position negativeOffset = new Position(1, 1, 1);
-				//Position negativeOffset = new Position(0, 0, 0);
-				//context.addDetail(context.createCuboid(position, end, parts, offset, negativeOffset, block));
-				//context.addDetail(context.createCuboid(p, end, material));
+
 				if (addAsSolid) context.addSolid(context.createCuboid(p, p,  parts, offset, negativeOffset, material));
 				else context.addDetail(context.createCuboid(p, p, parts, offset, negativeOffset, material));
-				//context.markAsConverted(p, end);
+
 				context.markAsConverted(p);
 			}
-			else {
+			else if (brushOptimized) {
 				// combined brushes together as one, looks bad but works well with
 				// transparency set to opaque
 				Position end = context.getCuboidFinder()
@@ -77,7 +78,13 @@ public class TreeLeaves extends Action {
 				if (!addAsSolid) context.addDetail(context.createCuboid(p, end, material));
 				else context.addSolid(context.createCuboid(p, end, material));
 				context.markAsConverted(p, end);
-
+			}
+			else if (!brushOptimized) {
+				// combined brushes together as one, looks bad but works well with
+				// transparency set to opaque
+				if (!addAsSolid) context.addDetail(context.createCuboid(p, p, material));
+				else context.addSolid(context.createCuboid(p, p, material));
+				context.markAsConverted(p);
 			}
 		}
 	}

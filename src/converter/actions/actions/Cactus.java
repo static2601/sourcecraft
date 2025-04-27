b9deaeb091275bd1db9803f6d234a37b7d6438aa
Statiic <staticx2601@yahoo.com>
2025-04-27 14:17:45 -0400

    Added trigger_hurt to cactus.

--- src/converter/actions/actions/Cactus.java
+++ src/converter/actions/actions/Cactus.java
@@ -4,6 +4,8 @@ import converter.actions.Action;
 import converter.mapper.Mapper;
 import minecraft.Block;
 import minecraft.Position;
+import vmfWriter.Solid;
+import vmfWriter.entity.solidEntity.TriggerHurt;
 
 public class Cactus extends Action {
 
@@ -15,6 +17,13 @@ public class Cactus extends Action {
 		Position offset = new Position(1, 0, 1);
 		Position negativeOffset = new Position(1, 1, 1);
 		context.addDetail(context.createCuboid(position, end, parts, offset, negativeOffset, block));
+
+		context.addSolidEntity(new TriggerHurt(this.createArea(
+				context, position, end, block), 1, 128));
+
 		context.markAsConverted(position, end);
+	}
+	private Solid createArea(Mapper context, Position p, Position end, Block material) {
+		return context.createCuboid(p, end, material);
 	}
 }
